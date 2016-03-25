19ddc0aa8c91c0f874998766b47d0c96eca6e2bf
AnyName <AnyEmail>
2016-03-25 09:18:39 +0700

    2

---
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package camt.se331.shoppingcart.repository;
+
+import camt.se331.shoppingcart.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Film on 3/25/2016.
+ */
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
