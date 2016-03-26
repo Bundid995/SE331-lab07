1eac8ee9556c72c5f9a0732e0108bd6ce45719fa
film <filmpurelove@gmail.com>
2016-03-27 05:09:48 +0700

    6.1

--- app/js/productService.js
+++ app/js/productService.js
@@ -20,3 +20,14 @@ productService.service('totalCalService',function() {
         return output;
     }
 })
+
+productService.factory('queryProductService',function($resource) {
+    return $resource('/getProduct/?name=:name',
+        {
+            query: {method: 'GET', params: {name: ''}, isArray: true}
+
+
+        })
+});
+
+
