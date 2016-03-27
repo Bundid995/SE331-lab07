a326abac370343a732ecbb2ffc9a78fb851dd1fd
kimkim <bundid_rakkham@cmu.ac.th>
2016-03-27 17:08:34 +0700

    6

--- src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -3,10 +3,14 @@ package camt.se331.shoppingcart.repository;
 import camt.se331.shoppingcart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Film on 3/25/2016.
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    public List<Product> findByNameLike(String name);
+        public List<Product> findByNameContaining(String name);
 
 }
